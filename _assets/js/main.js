a99a6490951cb929c15944ddce7d128f3cc4661f
Tim Mostert <resident@hotmail.co.za>
2017-10-23 22:56:13 +0200

    made changes and updated content

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -17,8 +17,9 @@
 $(document).ready(function () {
 	var mySwiper = new Swiper ('.swiper-container', {
 		speed: 1000,
-		autoplay: 5000,
+		autoplay: 6200,
 		effect: 'slide',
+
 		// If we need pagination
 		pagination: '.swiper-pagination',
 
@@ -49,6 +50,25 @@ $(document).ready(function () {
 	})
 
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
 
 (function($) {
 
